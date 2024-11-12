086cc75c8c8471e9c15b040d1eb9a0cdc9f49b66
supachaisu <180935381+supachaisu@users.noreply.github.com>
2024-11-12 21:45:41 +0700

    create prettier config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
